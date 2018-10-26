ab8615fc12a340f54f8d6d4ecb3a1a5b97d2de0b
Pavithran <pavithranrao.1512@gmail.com>
2018-10-25 20:00:35 -0400

    big countries

---
+++ big_countris.sql
@@ -0,0 +1,18 @@
+-- Write your MySQL query statement below
+select
+  name,
+  population,
+  area
+from
+  World
+where
+  population > 25000000
+union
+select
+  name,
+  population,
+  area
+from
+  World
+where
+  area > 3000000
